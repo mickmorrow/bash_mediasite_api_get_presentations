d53295b25af8467268696acb81c5cad6c3bdf6d5
mickmorrow <64139801+mickmorrow@users.noreply.github.com>
2021-02-25 12:21:23 +0000

    Create mediasite_api_get_presentations_1000.sh

---
+++ mediasite_api_get_presentations_1000.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x
+clear
+echo 'This script will list the first 1,000 presentations (id, title). Press any key to continue, or control+c to quit.'
+echo
+read
+echo 'Working ...'
+echo
+#
+source ./mediasite_api_config.sh
+#rm ''"$MEDIASITE_ROOT_URL"'_presentations_id_list_1000.txt'
+#rm ''"$MEDIASITE_ROOT_URL"'_presentations_id_title_list_1000.txt'
+curl -sX GET ''"$MEDIASITE_ROOT_URL"'/Api/v1/Presentations?$select=card&$skip=0&$top=1000' \
+     --header 'sfapikey: '"$MEDIASITE_SFAPIKEY"'' \
+     --header 'Content-Type: application/json; charset=utf-8' \
+     --header 'Authorization: Basic '"$MEDIASITE_API_USER_BASE64"'' \
+     | grep Id | awk  '{ print $4 }' FS='"' > presentations_id_list_1000.txt
+curl -sX GET ''"$MEDIASITE_ROOT_URL"'/Api/v1/Presentations?$select=card&$skip=0&$top=1000' \
+     --header 'sfapikey: '"$MEDIASITE_SFAPIKEY"'' \
+     --header 'Content-Type: application/json; charset=utf-8' \
+     --header 'Authorization: Basic '"$MEDIASITE_API_USER_BASE64"'' \
+     | grep Id | awk  '{ print $4,$8 }' FS='"' > presentations_id_title_list_1000.txt
+echo List Presentations ...
+cat presentations_id_title_list_1000.txt
+echo
+echo Total number of Presentations "$(wc -l presentations_id_title_list_1000.txt |awk '{print $1}')"
+echo
